5af57cada4e5505c69f8b564667084aa5becede1
DevWingley <dyinglightesmecher@yahoo.com>
2026-05-23 13:27:21 +0300

    Update meniu 2
    play - new game
            - continue
            - laod game
            - back button

--- Assets/Scripturi/MainMenuUI.cs
+++ Assets/Scripturi/MainMenuUI.cs
@@ -1,8 +1,27 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
     public GameObject optionsPanel;
+    public GameObject playPanel;
+    public GameObject mainButtons;
+
+    // PLAY PANEL
+
+    public void OpenPlayPanel()
+    {
+        playPanel.SetActive(true);
+        mainButtons.SetActive(false);
+    }
+
+    public void ClosePlayPanel()
+    {
+        playPanel.SetActive(false);
+        mainButtons.SetActive(true);
+    }
+
+    // OPTIONS
 
     public void OpenOptions()
     {
@@ -13,6 +32,25 @@ public class MainMenuUI : MonoBehaviour
     {
         optionsPanel.SetActive(false);
     }
+
+    // GAME
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void LoadGame()
+    {
+        Debug.Log("Load Game pressed");
+    }
+
+    // QUIT
 
     public void QuitGame()
     {
